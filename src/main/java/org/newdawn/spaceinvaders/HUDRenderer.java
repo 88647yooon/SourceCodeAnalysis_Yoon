a3c34f006c604a7d725e5b58cc0eb45c6f551647
ohdeahyun <eagleodh03@gmail.com>
2025-09-25 11:01:49 +0900

    플레이어 HP HUD를 구현했습니다. 한대맞으면 2데미지 들어오는 버그, emptyheart 안나오는 버그, 수정 부탁드립니다

---
+++ src/main/java/org/newdawn/spaceinvaders/HUDRenderer.java
@@ -0,0 +1,39 @@
+package org.newdawn.spaceinvaders;
+
+import org.newdawn.spaceinvaders.entity.ShipEntity;
+
+import java.awt.*;
+
+public class HUDRenderer {
+    private Game game;
+    public HUDRenderer(Game game){
+        this.game = game;
+    }
+
+    public void render(Graphics2D g){
+        ShipEntity player = game.getPlayerShip();
+        int x = 20, y = 20;
+        int HeartSize = 20;
+        //하트로 표시
+        Image heart = SpriteStore.get().getSprite("sprites/heart.png").getImage();
+        Image emptyheart = SpriteStore.get().getSprite("sprites/emptyheart.png").getImage();
+        for(int i=0;i<player.getCurrentHP(); i++){
+            if(i < player.getCurrentHP()){
+                //현재 HP 보다 작으면 ->빨간 하트
+                g.drawImage(heart,x + (i*(HeartSize+10)),y,HeartSize,HeartSize,null);
+            }
+            else{
+                // 나머지 -> 빈 하트
+                g.drawImage(emptyheart,x + (i*(HeartSize+10)),y,HeartSize,HeartSize,null);
+            }
+        }
+
+        /*
+        g.setFont(new Font("Arial", Font.BOLD, 16));
+        g.setColor(Color.BLACK);
+        //나중에 동규가 스코어 추가하면서 추가할 예정
+        //g.drawString("SCORE: " + game.getScore(), x, y+40);
+        */
+
+    }
+}
