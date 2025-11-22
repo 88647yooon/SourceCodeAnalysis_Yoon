b2f4cac118cdb3b3e4d075ead4974f2252e40f73
윤동 <88647yoon@gmail.com>
2025-11-22 17:47:28 +0900

    게임을 끝내는 것을 메뉴스크린이아닌 Game의 requsetExit()가 하도록 책임 분

--- src/main/java/org/newdawn/spaceinvaders/MenuScreen.java
+++ src/main/java/org/newdawn/spaceinvaders/MenuScreen.java
@@ -8,24 +8,28 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 
+import java.util.logging.Logger;
+
 public class MenuScreen implements Screen {
     private Game game;
     private BufferedImage background;
+    private final Logger logger = Logger.getLogger(MenuScreen.class.getName());
     private String[] menuItems = {"스테이지 모드", "무한 모드", "스코어보드", "게임 종료"};
     private int menuIndex = 0;
+
 
     public MenuScreen(Game game) {
         this.game = game;
         try {
             background = ImageIO.read(getClass().getResource("/sprites/Menu.png"));
         } catch (Exception e) {
-            System.err.println("메뉴 배경 로드 실패.." +e.getMessage());
+            logger.warning("메뉴 배경 로드 실패.." +e.getMessage());
         }
     }
 
     @Override
     public void render(Graphics2D g) {
-
+        final String DIALOG = "Dialog";
         // 1.배경
         if(background != null) {
             g.drawImage(background, 0, 0, 800,600, null);
@@ -37,12 +41,12 @@ public class MenuScreen implements Screen {
 
         //2.타이틀
         g.setColor(Color.white);
-        g.setFont(new Font("Dialog", Font.BOLD, 28));
+        g.setFont(new Font(DIALOG, Font.BOLD, 28));
         String title = "Space Invaders";
         g.drawString(title, (800 - g.getFontMetrics().stringWidth(title)) / 2, 150);
 
         // 3. 메뉴 항목
-        g.setFont(new Font("Dialog", Font.PLAIN, 22));
+        g.setFont(new Font(DIALOG, Font.PLAIN, 22));
         for (int i = 0; i < menuItems.length; i++) {
             String item = (i == menuIndex ? "> " : "  ") + menuItems[i];
             int x = (800 - g.getFontMetrics().stringWidth(item)) / 2;
@@ -50,7 +54,7 @@ public class MenuScreen implements Screen {
             g.drawString(item, x, y);
         }
         // 4. 도움말
-        g.setFont(new Font("Dialog", Font.ITALIC, 16));
+        g.setFont(new Font(DIALOG, Font.ITALIC, 16));
         String help = "↑/↓: 이동, ENTER: 선택, ESC: 종료";
         g.drawString(help, (800 - g.getFontMetrics().stringWidth(help))/ 2, 500);
     }
@@ -71,7 +75,9 @@ public class MenuScreen implements Screen {
                 case 0: game.setScreen(new StageSelectScreen(game)); break;
                 case 1: game.startInfiniteMode(); break;
                 case 2: game.showScoreboard(); break;
-                case 3: System.exit(0);
+                case 3: game.requestExit(); break;
+                default:
+                    logger.warning("Unkonwn state");
             }
         }
     }
